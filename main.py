1df216b79b57aaa4659576208bc2f8e5fdc7e6bc
Agnieszka Nowak <an450258@students.mimuw.edu.pl>
2024-03-09 17:09:39 +0100

    Skrypt dodaje do pliku produkty zakazane

--- main.py
+++ main.py
@@ -32,10 +32,19 @@ for el in headers_h2:
 # Odczytanie pierwszego akapitu strony
 akapit_wstep = soup.find('div', class_='entry-content').find('span')
 
+
+# Odczytanie przedostatniego akapitu strony
+podtytul_zakazane = soup.find('h2', string = headers_h2[-2].string)
+if podtytul_zakazane:
+    akapit_zakazane = podtytul_zakazane.find_next_sibling('p')
+    if akapit_zakazane:
+        lista_zakazanych = akapit_zakazane.find_next_sibling('ul')
+
+
 # Odczytanie ostatniego akapitu strony
-podtytul_podsumowanie = soup.find('h2', string = headers_h2[-1].string)
-if podtytul_podsumowanie:
-    akapit_podsumowanie = podtytul_podsumowanie.find_next_sibling('p')
+podtytul_dieta = soup.find('h2', string = headers_h2[-1].string)
+if podtytul_dieta:
+    akapit_podsumowanie = podtytul_dieta.find_next_sibling('p')
     if akapit_podsumowanie:
         lista_pokarmow = akapit_podsumowanie.find_next_sibling('ul')
 
@@ -49,3 +58,9 @@ with open('index.md', 'w', encoding='utf-8') as file:
             if tag:
                 file.write('* **' + tag.text.strip() + '**')
                 file.write(el.text.replace(tag.text, '') + '\n')
+    file.write('## ' + podtytul_zakazane.text + '\n')
+    file.write(akapit_zakazane.text + '\n')
+    if lista_zakazanych:
+        for el in lista_pokarmow.find_all('li'):
+            file.write('*' + el.text + '\n')
+
